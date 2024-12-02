9e05a6d4c40917fa34081271cad5dac84d70bc62
24Kings <24kingsunite@gmail.com>
2024-12-02 13:06:49 -0800

    Changed board size to default

--- hill_climbing_improved.cpp
+++ hill_climbing_improved.cpp
@@ -13,7 +13,7 @@ int minimum_remaining_values(ChessBoard board);
 int randomNum(int min, int max);
 void signalHandler(int signum);
 
-const int BOARD_SIZE = 16;
+const int BOARD_SIZE = 8;
 ChessBoard board(BOARD_SIZE);
 int restarts = 0;
 int h = 0;
